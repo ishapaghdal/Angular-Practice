142f17d13198e31ee9f85aa000fd06e1286f32ca
ishapaghdal <isha@auberginesolutions>
2025-02-14 14:38:49 +0530

    Stashed changes branch added

--- src/app/tasks.service.spec.ts
+++ src/app/tasks.service.spec.ts
@@ -18,7 +18,7 @@ describe('TasksService', () => {
     service.addTask('Test Task');
     expect(service.allTasks()).toEqual(['Test Task']);
   });
-  it('should not add a message',()=>{
+  it('should not add a messagexxxxxxxxx',()=>{
     service.addMessage('Test Message');
   })
 });
