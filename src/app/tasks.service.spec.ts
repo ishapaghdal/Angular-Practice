4d1e0797c4628d252b7c1032edfce7294bb59abb
ishapaghdal <isha@auberginesolutions>
2025-02-14 12:51:22 +0530

    Merge confilct resolved for tasks spec

---
+++ src/app/tasks.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TasksService } from './tasks.service';
+
+describe('TasksService', () => {
+  let service: TasksService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(TasksService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should add a task', () => {
+    service.addTask('Test Task');
+    expect(service.allTasks()).toEqual(['Test Task']);
+  });
+});
